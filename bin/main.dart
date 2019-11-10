36b7ef29c1fe246a7b608cf3f062df1ff0e342ba
Josh Jones <ohblonddev@gmail.com>
2019-11-10 23:28:29 +0000

    Initial Commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:empires_game/empires_game.dart' as empires_game;
+
+main(List<String> arguments) {
+  print('Hello world: ${empires_game.calculate()}!');
+}
---
+++ test/empires_game_test.dart
@@ -0,0 +1,8 @@
+import 'package:empires_game/empires_game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
